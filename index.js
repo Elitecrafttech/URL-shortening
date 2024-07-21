d0edd41183b24c58b66cd9f79b1b3989f098201d
Elitecrafttech <Elitecrafttech@gmail.com>
2024-07-21 05:13:23 -1100

    navigation

---
+++ index.js
@@ -0,0 +1,8 @@
+function tapopen(){
+    const list = document.querySelector(".list")
+    const unicode1 = document.querySelector(".unicode1")
+    const unicode2 = document.querySelector(".unicode2")
+    list.classList.toggle('scroll')
+    unicode1.classList.toggle('hidtg')
+    unicode2.classList.toggle('newtg')
+}
